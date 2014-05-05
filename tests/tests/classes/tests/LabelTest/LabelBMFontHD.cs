613daa87a9fc49ebcc40cfa13b5213779f9f2183
Jacob Anderson <jwa@totallyevil.com>
2014-05-05 14:19:22 -0700

    memory test number two for issue #414

--- tests/tests/classes/tests/LabelTest/LabelBMFontHD.cs
+++ tests/tests/classes/tests/LabelTest/LabelBMFontHD.cs
@@ -73,4 +73,52 @@ namespace tests
             return "Looking for memory leaks";
         }
     }
+
+    public class LabelBMFontHDMemoryLeak2 : AtlasDemo
+    {
+        public LabelBMFontHDMemoryLeak2()
+        {
+        }
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            ScheduleUpdate();
+        }
+
+        private float elapsed = 0f;
+        private CCNode label1;
+
+        public override void Update(float dt)
+        {
+            elapsed += dt;
+            if (elapsed > 0.5f)
+            {
+                elapsed = 0f;
+                // CCLabelBMFont
+                if (label1 != null)
+                {
+                    RemoveChild(label1);
+                }
+                CCNode node = new CCNode();
+                CCSize s = CCDirector.SharedDirector.WinSize;
+                float x = s.Width * CCMacros.CCRandomBetween0And1();
+                float y = s.Height * CCMacros.CCRandomBetween0And1();
+                label1 = new CCLabelBMFont(string.Format("{0:N2},{1:N2} @ Child Mem Leak", x, y), "fonts/konqa32.fnt");
+                node.AddChild(label1);
+                label1.Position = new CCPoint(x, y);
+                AddChild(node);
+                label1 = node;
+            }
+        }
+        public override string title()
+        {
+            return "Testing LabelBMFont";
+        }
+
+        public override string subtitle()
+        {
+            return "Looking for memory leaks with child label in node";
+        }
+    }
 }
