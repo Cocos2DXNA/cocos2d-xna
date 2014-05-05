eba4a4dae97ca5159c4ac54f2893a216cb30dfbe
Jacob Anderson <jwa@totallyevil.com>
2014-05-05 14:09:47 -0700

    Test case for memory leak issue #414

--- tests/tests/classes/tests/LabelTest/AtlasTestScene.cs
+++ tests/tests/classes/tests/LabelTest/AtlasTestScene.cs
@@ -9,7 +9,7 @@ namespace tests
     public class AtlasTestScene : TestScene
     {
         static int sceneIdx = -1;
-        static readonly int MAX_LAYER = 26;
+        static readonly int MAX_LAYER = 27;
 
         protected override void NextTestCase()
         {
@@ -83,6 +83,7 @@ namespace tests
                 case 23: return new TTFFontInit();
                 case 24: return new Issue1343();
                 case 25: return new GitHubIssue5();
+                case 26: return new LabelBMFontHDMemoryLeak();
                 default:
                     break;
             }
--- tests/tests/classes/tests/LabelTest/LabelBMFontHD.cs
+++ tests/tests/classes/tests/LabelTest/LabelBMFontHD.cs
@@ -28,4 +28,49 @@ namespace tests
             return "loading arista16 or arista16-hd";
         }
     }
+
+    public class LabelBMFontHDMemoryLeak : AtlasDemo
+    {
+        public LabelBMFontHDMemoryLeak()
+        {
+        }
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            ScheduleUpdate();
+        }
+
+        private float elapsed = 0f;
+        private CCLabelBMFont label1;
+
+        public override void Update(float dt)
+        {
+            elapsed += dt;
+            if (elapsed > 0.5f)
+            {
+                elapsed = 0f;
+                // CCLabelBMFont
+                if (label1 != null)
+                {
+                    RemoveChild(label1);
+                }
+                CCSize s = CCDirector.SharedDirector.WinSize;
+                float x = s.Width * CCMacros.CCRandomBetween0And1();
+                float y = s.Height * CCMacros.CCRandomBetween0And1();
+                label1 = new CCLabelBMFont(string.Format("{0:N2},{1:N2} @ MEMORY LEAK", x, y), "fonts/konqa32.fnt");
+                AddChild(label1);
+                label1.Position = new CCPoint(x, y);
+            }
+        }
+        public override string title()
+        {
+            return "Testing LabelBMFont";
+        }
+
+        public override string subtitle()
+        {
+            return "Looking for memory leaks";
+        }
+    }
 }
