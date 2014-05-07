b60b464eb871219433c60b81f23cbbc9a3350a38
Jacob Anderson <jwa@totallyevil.com>
2014-05-07 13:40:48 -0700

    fix for xbox 360 build.

--- cocos2d/platform/PList/PlistDocument.cs
+++ cocos2d/platform/PList/PlistDocument.cs
@@ -153,7 +153,23 @@ namespace Cocos2D
             // Read the asset into memory in one go. This results in a ~50% reduction
             // in load times on Android due to slow Android asset streams.
             MemoryStream memStream = new MemoryStream();
+#if XBOX360
+            byte[] buf = new byte[4096];
+            while (data.CanRead)
+            {
+                int amt = data.Read(buf, 0, buf.Length);
+                if (amt == -1)
+                {
+                    break;
+                }
+                if (amt > 0)
+                {
+                    memStream.Write(buf, 0, amt);
+                }
+            }
+#else
             data.CopyTo(memStream);
+#endif
             memStream.Seek(0, SeekOrigin.Begin);
             data.Close();
 
