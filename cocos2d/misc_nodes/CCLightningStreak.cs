bb7b74f68da35d47c2fa84801ac5fd34df5059b8
Jacob Anderson <jwa@totallyevil.com>
2014-05-19 12:18:45 -0700

    Fix for the strike streak not reaching the end point.

--- cocos2d/misc_nodes/CCLightningStreak.cs
+++ cocos2d/misc_nodes/CCLightningStreak.cs
@@ -12,6 +12,11 @@ namespace Cocos2D
         private bool _Cyclical = false;
         private float _StreakLifetime = 1f;
 
+        public CCLightningStreak()
+        {
+            BlendFunc = CCBlendFunc.Additive;
+        }
+
         public CCLightningStreak(CCPoint start, CCPoint end, float fadeTime, float minSegLength, float streakWidth, CCColor3B color, string pathToTexture)
             : base(fadeTime, minSegLength, streakWidth, color, pathToTexture)
         {
@@ -66,24 +71,35 @@ namespace Cocos2D
             }
         }
 
+        private bool _IsComplete = false;
+
         public override void Update(float delta)
         {
             _CurrentTime += delta;
             if (_CurrentTime > Duration)
             {
-                if (_Cyclical)
+                if (_IsComplete)
                 {
-                    // Automatic recycle?
-                    _CurrentTime = 0f;
+                    if (_Cyclical)
+                    {
+                        // Automatic recycle?
+                        _CurrentTime = 0f;
+                        _IsComplete = false;
+                    }
+                    else
+                    {
+                        // Don't update the position as this strike is just fading now
+                        base.Update(delta);
+                        return;
+                    }
                 }
                 else
                 {
-                    // Don't update the position as this strike is just fading now
-                    base.Update(delta);
-                    return;
+                    _CurrentTime = Duration;
+                    _IsComplete = true;
                 }
             }
-            Position = _Track.GetPoint(_CurrentTime);
+            Position = _Track.GetPoint(_CurrentTime / Duration);
             base.Update(delta);
         }
     }
